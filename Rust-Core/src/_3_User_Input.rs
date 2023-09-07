a6b32f6ab4e6ada6b32018066a649bddb84fa58f
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-07 20:35:44 +0530

    Refactor --> Added User input in Rust

--- Rust-Core/src/_3_User_Input.rs
+++ Rust-Core/src/_3_User_Input.rs
@@ -1,3 +1,10 @@
-fn main(){
+use std::io;
+
+fn main() {
+    let mut input = String::new();
     
+    /*This Imports the stdin() from io Package and read it uisng read_line (**works for string only) at reference of &mut in variable inout and retrun "Invalid Input" erro if given input is invalid */
+    io::stdin().read_line(&mut input).expect("Failed to read input");
+    
+    println!("{}", input);
 }
