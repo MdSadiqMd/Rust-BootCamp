b2cef6e69729bd776a8965919e24006e03aea338
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-09-19 19:30:41 +0530

    Feat: Updated loops

--- src/loops.rs
+++ src/loops.rs
@@ -1,5 +1,30 @@
 pub fn main() {
-    for i in 0..10{
+    /*  loop {
+        println!("This runs forever to stop this apply `break` keyword and to restart the loop use `continue`")
+    } */
+
+    // Multiple Loops --> If there are multiple loops are there we should give the loops labels and terminate them accordingly
+    //                --> If outer loop breaks innter loop will also break
+    /* let loops = 'outer_loop: loop {
+        if true {
+            loop {
+                break 'outer_loop 20; // This breaks outer loop
+                // break; // This breaks inner loop
+            }
+        }
+        break 10; // THis breaks outer loop
+    };
+    println!("{}", loops) */
+
+    // For loop
+    /* for i in 0..10{
         println!("{}",i)
+    } */
+
+    // While loop
+    let mut num = 5;
+    while num != 0 {
+        println!("{num}");
+        num = num - 1;
     }
 }
