dc4a2a83b370f8898c2e83d47838abb9caa00ff6
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-03-20 17:01:58 +0530

    🖨️ difference between {} and {:?}

--- src/practice/custom_macro.rs
+++ src/practice/custom_macro.rs
@@ -7,7 +7,28 @@ macro_rules! say_hello {
     };
 }
 
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: u8,
+}
+
+// custom macro implementation
+impl std::fmt::Display for Person {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} ({})", self.name, self.age)
+    }
+}
+
 pub fn main() {
     say_hello!();
     say_hello!("Sadiq");
+
+    // difference between {:?} and {}
+    let person = Person {
+        name: String::from("Sadiq"),
+        age: 30,
+    };
+    println!("{}", person); // normal printing --> we cannot print struct directly like this thus we create an custom macro implementation as above to make it print
+    println!("{:?}", person); // debug print, beautifully formatted print for debugging just need to add #[derive(Debug)] in the struct
 }
