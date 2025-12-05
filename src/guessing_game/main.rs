9e18490440f3fc1be204006203621edd10c12eff
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-12-05 23:40:38 +0530

    guess the number

--- src/guessing_game/main.rs
+++ src/guessing_game/main.rs
@@ -1,3 +1,26 @@
+use std::{cmp::Ordering, io};
+
 pub fn main() {
-    println!("Hello, world!");
+    println!("Guess the Number");
+    let secret_number = rand::random_range(1..=10);
+    loop {
+        println!("input the number");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("please enter the number");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too less"),
+            Ordering::Greater => println!("more"),
+            Ordering::Equal => {
+                println!("you made it");
+                break;
+            }
+        }
+    }
 }
